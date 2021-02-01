d69545d515bef6c13939d605ac250a9ab5efbc7c
txinkoa <pchincoa@gmail.com>
2021-02-01 21:35:12 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -10,7 +10,9 @@ let url = [
 ];
 
 let img;
-let sound;
+let confetti = [];
+let song;
+let slider;
 let puzz;
 let counter = 0;
 let button1, button2;
@@ -18,12 +20,16 @@ const mischenZeit = 10;
 
 
 // Array-Bildern initialisieren und laden
+// Song initialisieren und laden
 
 
 function preload() {
    img = loadImage(url[Math.floor(random(url.length))]);
+   song = loadSound('sound/Oh-by-jingo.mp3');
 
-}
+};
+
+
 
 // Puzzlebildern werden in Canvas geladen
 
@@ -31,24 +37,65 @@ function setup() {
    createCanvas(windowWidth, windowHeight);
    button1 = createButton('Reload the Puzzle');
    button1.mousePressed(nachLaden);
-
-
-  
-
+   song.play();
+   slider = createSlider(0,1,0,0.01);
 
 
 
-};
+   // Id auswählen und einstellen
+
+   let timer = select('#timer');
+   timer.html(convertSeconds(counter));
+   setInterval(timeIt, 800)
+
+   // Settings Timer
+
+   function timeIt() {
+      counter++;
+      timer.html(convertSeconds(counter));
+   }
+
+   // Confetti Piece Instance
+   // nach dem Lösung des Puzzle Confetti zeigen
+
+   for (let i = 0; i < 800; i++) {
+      confetti[i] = new Confetti();
+
+   }; // Ende Schleife
+
+
+
+}; //ENDE setup
 
 // Puzzlebildern werden in Canvas geladen
 // Bg Farbe, Stellung der Bilder und Frame Funktionen,
 
 function draw() {
    img.setFrame(frameCount % img.numFrames());
-   frameRate(5);
+   frameRate(8);
+   song.setVolume(slider.value());
    background(197, 204, 193);
    image(img, width / 3, height / 8, 700, 750);
+   // Confetti Schleife
+   for (let i = 0; i < 500; i++) {
+      //confetti[i].show();
+      confetti[i].updateConfetti();
 
+   }; // Ende Schleife
+
+
+
+
+
+}; //ENDE draw
+
+
+// Sekunden in "zweistellig" konvertieren
+
+function convertSeconds(s) {
+   let min = floor(s / 60);
+   let sec = s % 60;
+   return nf(min, 2) + ':' + nf(sec, 2);
 
 };
 
@@ -57,12 +104,10 @@ function draw() {
 function nachLaden() {
    button1 = window.location.reload();
 
-}
+};
 
 // Größe des Fensters ändern
 
 function windowResized() {
    resizeCanvas(windowWidth, windowHeight);
-}
-
-
+};
